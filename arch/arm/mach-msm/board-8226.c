1760ec083c412ccfb9ae65f829293c648d5dd161
Paul Reioux <reioux@gmail.com>
2013-11-02 19:07:23 -0500

    intelli/msm_thermal: initial coding for Qualcomm Snapdragon 800 devices
    an intelligent MSM thermal throttling kernel driver designed to replace the
    closed sourced thermald(aemon) from Qualcomm.
    
    The current driver can throttle the SOC via core frequency scaling or via
    core hotplug with user configurable sysfs entries to adjust the throttling
    temperature thresholds for each.
    
    Derived from Qualcomm's Open source repo.
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    Conflicts:
    
    	arch/arm/mach-msm/lge/board-8974-hammerhead.c
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    Conflicts:
    	drivers/thermal/Makefile

--- arch/arm/mach-msm/board-8226.c
+++ arch/arm/mach-msm/board-8226.c
@@ -126,7 +126,9 @@ void __init msm8226_add_drivers(void)
 	else
 		msm_clock_init(&msm8226_clock_init_data);
 	tsens_tm_init_driver();
+#ifndef CONFIG_INTELLI_THERMAL
 	msm_thermal_device_init();
+#endif
 }
 
 void __init msm8226_init(void)
