9b23a175a57a0098a0d6a3e7de4344e544f4bbdf
Paul Reioux <reioux@gmail.com>
2013-12-23 22:00:34 -0800

    intelli_plug.c: enable touch boost for Moto G touch screen
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- arch/arm/mach-msm/intelli_plug.c
+++ arch/arm/mach-msm/intelli_plug.c
@@ -357,6 +357,7 @@ static void intelli_plug_input_event(struct input_handle *handle,
 static int input_dev_filter(const char *input_dev_name)
 {
 	if (strstr(input_dev_name, "touchscreen") ||
+		strstr(input_dev_name, "synaptics_dsx_i2c") ||
 		strstr(input_dev_name, "sec_touchscreen") ||
 		strstr(input_dev_name, "touch_dev") ||
 		strstr(input_dev_name, "-keypad") ||
