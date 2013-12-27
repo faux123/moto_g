fe4b706ceb76ff144ba2f90f44583ab03042a886
Kiet Lam <kietl@codeaurora.org>
2013-12-27 15:42:11 -0800

    wlan: adding check for ts_info.up.
    'params' is user controlled, and 'ts_info.up' could be defined to be
    greater than maximum sme_QosWmmUpType enum, resulting in hddWmmUpToAcMap
    being indexed out of bounds.
    Validated 0 < ts_info.up < SME_QOS_WMM_UP_MAX.
    
    Change-Id: Ia1966db2a6c8ce8d3a9422f0dd9d91a59573c783
    CRs-Fixed: 567234
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- drivers/staging/prima/CORE/HDD/src/wlan_hdd_wext.c
+++ drivers/staging/prima/CORE/HDD/src/wlan_hdd_wext.c
@@ -5126,6 +5126,11 @@ static int iw_add_tspec(struct net_device *dev, struct iw_request_info *info,
       return 0;
    }
    tSpec.ts_info.up = params[HDD_WLAN_WMM_PARAM_USER_PRIORITY];
+   if(0 > tSpec.ts_info.up || SME_QOS_WMM_UP_MAX < tSpec.ts_info.up)
+   {
+   hddLog(VOS_TRACE_LEVEL_ERROR,"***ts_info.up out of bounds***");
+   return 0;
+   }
 
    if ((tSpec.ts_info.up == SME_QOS_WMM_UP_VO) ||
        (tSpec.ts_info.up == SME_QOS_WMM_UP_NC))
